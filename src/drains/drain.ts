7fe375a0320effa03074f5d5b22398ad57424803
Sebastian Vollbrecht <sebivo@freenet.de>
2025-02-04 09:38:41 +0100

    Rename type parameters

--- src/drains/drain.ts
+++ src/drains/drain.ts
@@ -4,12 +4,12 @@ import type { TestResults } from "../models/test-results-model.js";
  * A drain represents a service or tool to which test information can be pushed, such as Slack,
  * Microsoft Teams or simply the standard output.
  */
-export interface Drain<Output> {
+export interface Drain<DrainOutput> {
   /**
    * Writes test results, containing one or more tests and their results.
    *
    * @param results the results to write
    * @returns the drained result
    */
-  writeTestResults(results: TestResults): Promise<Output>;
+  writeTestResults(results: TestResults): Promise<DrainOutput>;
 }
--- src/sources/source.ts
+++ src/sources/source.ts
@@ -4,12 +4,12 @@ import type { TestResults } from "../models/test-results-model.js";
  * A source represents a service or tool from which test information can be pulled, such as an Xray
  * test execution, an Azure DevOps test run or a simple Excel sheet.
  */
-export interface Source<Filter> {
+export interface Source<SourceDetails> {
   /**
    * Retrieves test results from the source, containing one or more tests and their results.
    *
    * @param source the source to retrieve results from
    * @returns the test results
    */
-  getTestResults(source: Filter): Promise<TestResults> | TestResults;
+  getTestResults(source: SourceDetails): Promise<TestResults> | TestResults;
 }
