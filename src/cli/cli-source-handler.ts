8e755e3153c4b12c394cf06c6049b2ed66a38595
Sebastian Vollbrecht <sebivo@freenet.de>
2025-02-05 19:39:37 +0100

    Improve handler documentation

--- src/cli/cli-source-handler.ts
+++ src/cli/cli-source-handler.ts
@@ -5,28 +5,86 @@ import type { Source } from "../sources/source.js";
  * partial configurations. It is also responsible for restoring method parameters, such that a
  * source may be built and immediately used in a programmatic fashion.
  */
-export interface SourceHandler<S extends Source<unknown>, C, P = Parameters<S["getTestResults"]>> {
+export abstract class SourceHandler<
+  S extends Source<unknown>,
+  C,
+  P = Parameters<S["getTestResults"]>,
+> {
   /**
-   * Builds the source from scratch.
+   * Creates and returns a fully initialised source instance. The source can be generated from
+   * environment variables or interactively created using packages such as
+   * [`@inquirer/prompts`](https://www.npmjs.com/package/@inquirer/prompts).
    *
-   * @returns the source
+   * @returns the source instance
    */
-  buildSource: () => Promise<S>;
+  public abstract buildSource(): Promise<S> | S;
+
   /**
-   * Restores a source from a saved configuration. The configuration may be incomplete and may
-   * require additional input, which is up to the handler implementation.
+   * Serializes the given source instance into a format suitable for storage. The result must be
+   * JSON serializable and need not include all source details. Sensitive values should be omitted
+   * or replaced with reconstructible placeholders (e.g., authentication details may be encoded as
+   * `{ authentication: "basic" }` instead of storing the credentials in cleartext).
    *
-   * @param savedConfiguration the saved configuration
-   * @returns the source
+   * The actual serialization format is up to the implementation. Anything goes, as long as the
+   * corresponding deserialization method is able to accurately reconstruct the source.
+   *
+   * @param source the source instance to serialize
+   * @returns the JSON-serializable serialized source
    */
-  restoreSource: (savedConfiguration: C) => Promise<S> | S;
+  public abstract serializeSource(source: S): C | Promise<C>;
+
   /**
-   * Restores a source test results call from a (subset of) saved parameters.
+   * Restores a source instance from a previously serialized configuration. The returned
+   * configuration may be incomplete and require additional input during deserialization.
    *
-   * @param savedParameters the saved parameters
-   * @returns the complete parameters to use for a test results retrieval
+   * The implementation must be able to restore what has been produced by `serializeSource`. Any
+   * missing or omitted details must be filled in from external sources (e.g. environment variables
+   * or user input).
+   *
+   * @param serializedSource the serialized source
+   * @returns the restored source
    */
-  restoreSourceParameters: (
-    savedParameters: P
-  ) => Parameters<S["getTestResults"]> | Promise<Parameters<S["getTestResults"]>>;
+  public abstract deserializeSource(serializedSource: C): Promise<S> | S;
+
+  /**
+   * Constructs and returns the parameters required for retrieving test results. Parameters can be
+   * generated from environment variables or interactively created using packages such as
+   * [`@inquirer/prompts`](https://www.npmjs.com/package/@inquirer/prompts).
+   *
+   * @returns the parameters
+   */
+  public abstract buildSourceParameters():
+    | Parameters<S["getTestResults"]>
+    | Promise<Parameters<S["getTestResults"]>>;
+
+  /**
+   * Serializes the given source retrieval parameters into a format suitable for storage. The result
+   * must be JSON serializable and need not include all source details. Sensitive values should be
+   * omitted or replaced with reconstructible placeholders (e.g., authentication details may be
+   * encoded as `{ authentication: "basic" }` instead of storing the credentials in cleartext).
+   *
+   * The actual serialization format is up to the implementation. Anything goes, as long as the
+   * corresponding deserialization method is able to accurately reconstruct the parameters.
+   *
+   * @param parameters the complete parameters
+   * @returns the JSON-serializable parameters
+   */
+  public abstract serializeSourceParameters(
+    parameters: Parameters<S["getTestResults"]>
+  ): P | Promise<P>;
+
+  /**
+   * Restores test result parameters from a previously serialized configuration. The returned
+   * parameters may be incomplete and require additional input during deserialization.
+   *
+   * The implementation must be able to restore what was produced by `serializeSourceParameters`.
+   * Any missing or omitted details must be filled in from external sources (e.g. environment
+   * variables or user input).
+   *
+   * @param serializedParameters the serialized parameters
+   * @returns the restored parameters
+   */
+  public abstract deserializeSourceParameters(
+    serializedParameters: P
+  ): Parameters<S["getTestResults"]> | Promise<Parameters<S["getTestResults"]>>;
 }
