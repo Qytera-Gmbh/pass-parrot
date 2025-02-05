c30d41066858aaa10b82fb50290df6dae0dae1d4
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2025-02-05 17:36:36 +0100

    Add handler documentation

--- src/cli/cli-source-handler.ts
+++ src/cli/cli-source-handler.ts
@@ -1,8 +1,31 @@
 import type { Source } from "../sources/source.js";
 
+/**
+ * A source handler is responsible for building a source from scratch and for restoring sources from
+ * partial configurations. It is also responsible for restoring method parameters, such that a
+ * source may be built and immediately used in a programmatic fashion.
+ */
 export interface SourceHandler<S extends Source<unknown>, C, P = Parameters<S["getTestResults"]>> {
+  /**
+   * Builds the source from scratch.
+   *
+   * @returns the source
+   */
   buildSource: () => Promise<S>;
+  /**
+   * Restores a source from a saved configuration. The configuration may be incomplete and may
+   * require additional input, which is up to the handler implementation.
+   *
+   * @param savedConfiguration the saved configuration
+   * @returns the source
+   */
   restoreSource: (savedConfiguration: C) => Promise<S> | S;
+  /**
+   * Restores a source test results call from a (subset of) saved parameters.
+   *
+   * @param savedParameters the saved parameters
+   * @returns the complete parameters to use for a test results retrieval
+   */
   restoreSourceParameters: (
     savedParameters: P
   ) => Parameters<S["getTestResults"]> | Promise<Parameters<S["getTestResults"]>>;
